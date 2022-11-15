394b85029668c349528bf8452262dbb750966b80
Arnold Kibira <arnold.ril3y@gmail.com>
2022-11-16 00:37:38 +0300

    set up base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "first-hero": "url('/assets/images/hero.png')",
+      },
+      colors: {
+        "arch-gray": "#2C2C2C",
+        "text-primary": "#BDBDBD",
+        "text-secondary": "#333333",
+      },
+      fontFamily: {
+        hoves: ["TThoves", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
